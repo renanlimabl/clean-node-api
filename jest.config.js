bc4135d8d33b5bd2465b2eec8e68117986a0a6bd
renanlimabl <renanlimabl@gmail.com>
2021-01-11 22:14:35 -0300

    chore: adiciona jest and configurations

---
+++ jest.config.js
@@ -0,0 +1,12 @@
+module.exports = {
+  roots: [
+    '<rootDir>/src',
+  ],
+  collectCoverageFrom: ['<rootDir>/src/**/*.ts'],
+  coverageDirectory: 'coverage',
+  testEnvironment: 'node',
+  coverageProvider: 'v8',
+  transform: {
+    '.+\\.ts$': 'ts-jest',
+  },
+};
